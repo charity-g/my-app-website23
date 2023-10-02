4bebb05c11fc5670d2c8483fca4e93dfaf518676
Charity <charity.grey1@gmail.com>
2023-10-01 20:33:35 -0700

    path breakage testing 1

--- src/Components/pages/About.js
+++ src/Components/pages/About.js
@@ -4,6 +4,7 @@ import React from 'react';
 function About() {
     return (
         <> 
+            <img src='images/IMG_CharityGrey.jpg'></img>
             <h1>charity grey</h1>
             <img src='images/IMG_CharityGrey.jpg'></img>
         </>
