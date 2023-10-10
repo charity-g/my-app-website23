4ac6c4afcc9afb378e7245c4031cc9dd2f2085b6
Charity <charity.grey1@gmail.com>
2023-10-09 19:07:21 -0700

    relative image path test2

--- src/Components/pages/About.js
+++ src/Components/pages/About.js
@@ -5,7 +5,7 @@ function About() {
     return (
         <> 
             <img src='https://www.w3schools.com/images/w3schools_green.jpg'></img>
-            <img src={require('/images/IMG_CharityGrey.jpg')}></img>
+            <img src={'/images/IMG_CharityGrey.jpg'}></img>
             <h1>Hi! My name is Charity.</h1>
         </>
     )
