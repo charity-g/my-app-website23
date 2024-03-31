78da4d901bd06fb80f46f3e0fb40136fec828cb0
Charity <charity.grey1@gmail.com>
2024-03-30 17:43:03 -0700

    Implemented Surprise

--- src/Components/pages/surprise.js
+++ src/Components/pages/surprise.js
@@ -1,11 +1,23 @@
+import '../../App.css';
+import React from 'react';
+import './Surprise.css';
 import video_path from "./surprise1.mp4";
+//<img src="https://source.unsplash.com/random?nature"></img>
+      
 
 function Surprise() {
    return (
-       <div className="media-container">
-         <video src={video_path} alt='You get rickrolled!'></video>
-       </div>
-   )
+      <>
+      <div className='media_container'>
+      <video width="320" height="240" autoPlay={true} muted>
+            <source src={video_path} type="video/mp4"/>
+            Your browser does not support the video tag.
+      </video> 
+      <p>You got Rick Rolled!</p>
+
+      </div>
+      </>
+   );
 }
 
 export default Surprise;
